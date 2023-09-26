bab73eb0e27e684dd7bd477f3f568ae7af46e5d5
mert-unsal <mertusal0@gmail.com>
2023-09-27 01:39:15 +0300

    Initial commit

---
+++ src/main/java/com/munsal/java21demo/Java21demoApplication.java
@@ -0,0 +1,13 @@
+package com.munsal.java21demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Java21demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Java21demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/munsal/java21demo/Java21demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.munsal.java21demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java21demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
